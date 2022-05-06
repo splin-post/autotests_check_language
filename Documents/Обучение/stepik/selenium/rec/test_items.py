e344fbc80bfecd98c50503284f20d9efcdb093a5
Sergei P <spin.el@yandex.ru>
2022-05-06 19:49:37 +0300

    first commit

---
+++ Documents/Обучение/stepik/selenium/rec/test_items.py
@@ -0,0 +1,10 @@
+import time
+
+
+def test_should_be_visible_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_css_selector('button.btn.btn-lg.btn-primary.btn-add-to-basket')
+    btn_text = button.text
+    assert btn_text == 'Añadir al carrito'
