f2423d2481e9cccde568c53724bec6fad5ebe8c2
splin-post <98468396+splin-post@users.noreply.github.com>
2022-05-06 20:01:29 +0300

    Update test_items.py
    change the delay to 15 sec

--- Documents/Обучение/stepik/selenium/rec/test_items.py
+++ Documents/Обучение/stepik/selenium/rec/test_items.py
@@ -4,7 +4,7 @@ import time
 def test_should_be_visible_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(30)
+    time.sleep(15)
     button = browser.find_element_by_css_selector('button.btn.btn-lg.btn-primary.btn-add-to-basket')
     btn_text = button.text
     assert btn_text == 'Añadir al carrito'
